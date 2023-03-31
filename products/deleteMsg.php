c1a644ba15a8b0dce66d4545b16db5f73dacfd87
FelicianoNeuara <114292658+FelicianoNeuara@users.noreply.github.com>
2023-03-31 21:07:30 +0200

    Trabalho

---
+++ products/deleteMsg.php
@@ -0,0 +1,32 @@
+<?php
+$BASE_URL = './../';
+include_once __DIR__ . "/../header.php"
+?>
+<main id="main" class="main">
+
+  <div class="pagetitle">
+    <h1>Dashboard</h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+        <li class="breadcrumb-item active">Dashboard</li>
+      </ol>
+    </nav>
+  </div><!-- End Page Title -->
+
+  <section class="section dashboard">
+    <!-- INICIO -->
+    <div class="alert alert-danger alert-dismissible">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+      Produto Eliminado com sucessso.
+    </div>
+    <div class="text-center">
+      <a href="products/index.php"><button type="button" class="btn btn-sm btn-primary"><i class="fa fa-arrow-left"></i> Voltar</button></a>
+    </div>
+    <!-- FIM -->
+</main>
+
+
+<?php
+include_once __DIR__ . '/../footer.php'
+?>
---
+++ products/index.php
@@ -0,0 +1,118 @@
+<?php
+include_once __DIR__ . "/../db.php";
+
+$BASE_URL = './../';
+include_once __DIR__ . "/../header.php"
+?>
+<main id="main" class="main">
+
+  <div class="pagetitle">
+    <h1>Dashboard</h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+        <li class="breadcrumb-item active">Dashboard</li>
+      </ol>
+    </nav>
+  </div><!-- End Page Title -->
+
+  <section class="section">
+    <div class="row">
+      <div class="col-lg-12">
+
+        <div class="card">
+          <div class="card-body">
+            <div class="d-flex align-items-center justify-content-between">
+              <h5 class="card-title">Produtos</h5>
+
+              <a href="products/add.php">
+                <button type="button" class="btn btn-primary">Adicionar</button>
+              </a>
+            </div>
+
+            <!-- Table with stripped rows -->
+            <div class="datatable-wrapper datatable-loading no-footer sortable searchable fixed-columns">
+              <div class="datatable-top">
+                <div class="datatable-dropdown">
+                  <label>
+                    <select class="datatable-selector">
+                      <option value="5">5</option>
+                      <option value="10" selected="">10</option>
+                      <option value="15">15</option>
+                      <option value="20">20</option>
+                      <option value="25">25</option>
+                    </select> entries per page
+                  </label>
+                </div>
+                <div class="datatable-search">
+                  <input class="datatable-input" placeholder="Search..." type="search" title="Search within table">
+                </div>
+              </div>
+              <div class="datatable-container">
+                <table class="table datatable datatable-table">
+                  <thead>
+                    <tr>
+                      <th data-sortable="true" style="width: 5.690200210748156%;"><a href="#" class="datatable-sorter">#</a></th>
+                      <th data-sortable="true" style="width: 28.02950474183351%;"><a href="#" class="datatable-sorter">Código</a></th>
+                      <th data-sortable="true" style="width: 37.72391991570074%;"><a href="#" class="datatable-sorter">Nome</a></th>
+                      <th data-sortable="true" style="width: 9.272918861959958%;"><a href="#" class="datatable-sorter">Preço</a></th>
+                      <th data-sortable="true" style="width: 9.272918861959958%;"><a href="#" class="datatable-sorter">Acção</a></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    // faz a seleção do dados na Tabela	
+                    $data = mysqli_query($connection, "SELECT * FROM products ORDER BY id ASC");
+                    // lista os dados
+                    while ($value = mysqli_fetch_array($data)) {
+
+                      // PDO
+                      // $stmt = $pdo->prepare("SELECT * FROM users ORDER BY id DESC");
+                      // $stmt->execute();
+                      // $values = $stmt->fetchAll();
+
+                      // foreach($values as $value) {
+                    ?>
+                      <tr data-index="0">
+                        <td><?php echo $value['id'] ?></td>
+                        <td><?php echo $value['cod'] ?></td>
+                        <td><?php echo $value['name'] ?></td>
+                        <td><?php echo $value['price'] ?></td>
+                        <td>
+
+
+
+                          <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <a href="products/edit.php?id=<?php echo $value['id'] ?>">
+                              <button type="button" class="btn btn-primary">Editar</button>
+                            </a>
+                            <a href="products/deleteQuery.php?id=<?php echo $value['id'] ?>">
+                              <button type="button" class="btn btn-danger">Deletar</button>
+                            </a>
+                          </div>
+
+                        </td>
+                      </tr>
+                    <?php }  ?>
+                  </tbody>
+                </table>
+              </div>
+              <div class="datatable-bottom">
+                <div class="datatable-info">Showing 1 to 5 of 5 entries</div>
+                <nav class="datatable-pagination">
+                  <ul class="datatable-pagination-list"></ul>
+                </nav>
+              </div>
+            </div>
+            <!-- End Table with stripped rows -->
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+</main><!-- End #main -->
+
+<?php include_once __DIR__ . "/../footer.php" ?>
